501a43615780435cef6874015b4ec79ae4937102
Antonio <jconradolopez19@gmail.com>
2025-02-07 17:03:28 -0600

    Add types and schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,31 +1,57 @@
-import {z} from 'zod'
-//schema
+import { SvgIconComponent } from '@mui/icons-material';
+import { z } from 'zod';
+//schema and type User
 export const UserAuthSchema = z.object({
     id: z.number(),
     email: z.string(),
     status: z.boolean(),
-    roleId: z.number()
-})
+    roleId: z.number(),
+});
 
-//type
-export type User = {
-    id: number;
-    roleId: number;
-    name: string;
-    surname: string;
-    telephone: string | null;
-    email: string;
-    password: string;
-    image: string | null;
-    token: string | null;
-    isConfirm: boolean;
-    status: boolean;
-}
+export const RolSchema = z.object({
+    name: z.string(),
+});
 
-export type AuthForm = Pick<User, 'name' | 'surname' | 'email' | 'password' |  'telephone'> & { repeatPassword?: string }
+export type User = z.infer<typeof UserSchema>;
+export type AuthForm = Pick<
+    User,
+    'name' | 'surname' | 'email' | 'telephone'
+> & { repeatPassword?: string; password: string; token: string };
+export type userAuthType = z.infer<typeof UserAuthSchema>;
 
+export const UserSchema = z.object({
+    id: z.number(),
+    roleId: z.number(),
+    name: z.string(),
+    surname: z.string(),
+    telephone: z.string().nullable(),
+    email: z.string().email(),
+    image: z.string().nullable(),
+    roles: RolSchema,
+});
+
+//schema and type businessData
+export const BusinessDataSchema = z.object({
+    id: z.number(),
+    ruc: z.string(),
+    name: z.string(),
+    description: z.string(),
+    telephone: z.string(),
+    email: z.string().email().nullable(),
+    image: z.string().nullable(),
+});
+
+export type BusinessData = z.infer<typeof BusinessDataSchema>;
+
+//schema and type generals
 export type ResponseMsgAPI = {
-    msg: string; 
-}
-export type userAuthType = z.infer<typeof UserAuthSchema>
+    msg: string;
+};
 
+export type LinksSideBar = {
+    name: string;
+    path: string;
+    icon: SvgIconComponent;
+    rol?: string[];
+    links?: LinksSideBar[];
+};
