9a13f4e097c24bb64df323a8367968f8bd4bb2a9
Antonio <jconradolopez19@gmail.com>
2025-02-09 19:57:30 -0600

    API request

--- src/api/businessData/businessData.ts
+++ src/api/businessData/businessData.ts
@@ -1,11 +1,53 @@
 import api from '@/config/axios';
-import { BusinessDataSchema } from '@/types/index';
+import {
+    BusinessData,
+    BusinessDataSchema,
+    ResponseMsgAPISchema,
+} from '@/types/index';
 import { isAxiosError } from 'axios';
 
 export async function getBusinessDataAPI() {
     try {
         const { data } = await api('/businessData');
         const result = BusinessDataSchema.safeParse(data);
+        if (result.success) {
+            return result.data;
+        }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function updateBusinessDataAPI(formData: BusinessData) {
+    try {
+        const { data } = await api.put<string>('/businessData', formData);
+        return data;
+    } catch (error) {
+        console.log(error);
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function uploadImageBusinessDataAPI(image: File) {
+    try {
+        // Create a FormData object and append the image to the request body
+        const formData = new FormData();
+        formData.append('image', image); // 'image' should match the field name in the backend
+
+        const { data } = await api.post(
+            '/businessData/upload-image',
+            formData, // Use FormData instead of 'image' directly
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data', // This is necessary for file uploads
+                },
+            },
+        );
+        const result = ResponseMsgAPISchema.safeParse(data);
         if (result.success) {
             return result.data;
         }
