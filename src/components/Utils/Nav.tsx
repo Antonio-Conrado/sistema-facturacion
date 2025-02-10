51f1f7484b7f753ad13e5086a1db46f2e89eac7a
Antonio <jconradolopez19@gmail.com>
2025-02-09 19:59:57 -0600

    updated styles css

--- src/components/Utils/Nav.tsx
+++ src/components/Utils/Nav.tsx
@@ -9,7 +9,7 @@ type NavProps = {
 export default function Nav({ toggleSidebar, isLargeScreen }: NavProps) {
     return (
         <nav
-            className={`fixed z-50 w-full top-2 h-20 flex px-5 items-center shadow-lg bg-neutral-50 rounded-xl md:pr-64 lg:pr-80 ${
+            className={`fixed z-50 w-[calc(100%-16px)] lg:w-[calc(100%-253px)] top-2 h-20 flex px-5 items-center shadow-lg bg-neutral-50 rounded-xl ${
                 isLargeScreen ? 'justify-end' : 'justify-between'
             }`}
         >
