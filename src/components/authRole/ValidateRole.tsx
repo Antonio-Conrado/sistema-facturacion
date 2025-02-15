d09daf9d6b51310f1e2839e7e2a4e7a85d5fb47f
Antonio <jconradolopez19@gmail.com>
2025-02-15 10:52:25 -0600

    formatted code

--- src/components/authRole/ValidateRole.tsx
+++ src/components/authRole/ValidateRole.tsx
@@ -1,18 +1,17 @@
-import useAuth from "@/hooks/useAuth"
-import { Navigate } from "react-router-dom"
+import useAuth from '@/hooks/useAuth';
+import { Navigate } from 'react-router-dom';
 
 type ValidateRoleProps = {
-    children: React.ReactNode
-    roles: number[]
-}
+    children: React.ReactNode;
+    roles: number[];
+};
 
 export default function ValidateRole({ children, roles }: ValidateRoleProps) {
-    const {userAuth} = useAuth()
-    const hasRole = roles ? roles.includes(userAuth.roleId) : false
-    if(!hasRole){
-        <Navigate to={'/401'} />
-        return
+    const { userAuth } = useAuth();
+    const hasRole = roles ? roles.includes(userAuth.roleId) : false;
+    if (!hasRole) {
+        return <Navigate to={'/401'} />;
     }
 
-    return children
+    return children;
 }
