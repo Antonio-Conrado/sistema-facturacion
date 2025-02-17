a48068073fd202b204e75731300404f6ecb27eb2
Antonio <jconradolopez19@gmail.com>
2025-02-17 14:46:39 -0600

    updated styles css

--- src/components/Utils/DataNotFound.tsx
+++ src/components/Utils/DataNotFound.tsx
@@ -2,9 +2,9 @@ import { DoNotDisturb } from '@mui/icons-material';
 
 export default function DataNotFound() {
     return (
-        <div className="flex justify-center items-center gap-3 min-h-[80%]">
-            <DoNotDisturb sx={{ fontSize: '20px' }} />
-            <p className="text-center text-gray-800 text-3xl">
+        <div className="flex justify-center items-center gap-3 min-h-[90vh]">
+            <DoNotDisturb sx={{ fontSize: '50px' }} />
+            <p className="text-center text-gray-800 text-xl">
                 La información no está disponible. Intenta nuevamente o más
                 tarde!
             </p>
