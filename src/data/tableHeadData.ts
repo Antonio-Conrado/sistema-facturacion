09f89f2c917c3dcb0800c5e1bbea7b4dbf4efef3
Antonio <jconradolopez19@gmail.com>
2025-02-17 15:42:31 -0600

    updated data

--- src/data/tableHeadData.ts
+++ src/data/tableHeadData.ts
@@ -9,10 +9,22 @@ export const listSecurityHead = [
     'Acciones',
 ];
 
+//catalogs
 export const listCategoryHead = [
     'Id',
     'Nombre',
     'Descripción',
+    'Estado',
+    'Acciones',
+];
+
+export const listSuppliersHead = [
+    'Id',
+    'Ruc',
+    'Nombre',
+    'Dirección',
+    'Teléfono',
+    'Email',
     'Estado',
     'Acciones',
 ];
