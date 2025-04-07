cc5e81dae63b2ff3711601561bc1c730cf5cc230
Antonio <jconradolopez19@gmail.com>
2025-04-06 18:07:02 -0600

    updates msg

--- src/components/Utils/NoDataMessage.tsx
+++ src/components/Utils/NoDataMessage.tsx
@@ -10,7 +10,11 @@ export default function NoDataMessage({
     return (
         <TableRow>
             <TableCell align="center" colSpan={cols}>
-                No hay información disponible. Empieza registrando {msg}
+                <TableCell align="center" colSpan={cols}>
+                    No hay resultados para mostrar. Es posible que aún no se
+                    hayan registrado datos de {msg}, o que los filtros aplicados
+                    no coincidan con ningún registro.
+                </TableCell>
             </TableCell>
         </TableRow>
     );
