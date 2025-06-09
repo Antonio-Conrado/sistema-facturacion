95e402d29d712122256023a8296fdb71c66ddc4d
Antonio-Conrado <jconradolopez19@gmail.com>
2025-06-09 16:01:22 -0600

    validate if exists role

--- src/views/auth/LoginViews.tsx
+++ src/views/auth/LoginViews.tsx
@@ -1,6 +1,5 @@
-
-import { useForm } from 'react-hook-form'
-import { useMutation } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
 import { Link, useNavigate } from 'react-router-dom';
 import { login } from '@/api/auth/auth';
 import useToast from '@/hooks/useNotifications';
@@ -9,17 +8,23 @@ import InputEmail from '@/components/authForm/InputEmail';
 import InputPassword from '@/components/authForm/InputPassword';
 import Spinner from '@/components/Utils/Spinner';
 import useAuth from '@/hooks/useAuth';
+import { Role } from '@/data/index';
 
 export default function LoginViews() {
-    const navigate = useNavigate()
-    const toast = useToast()
-    const { setTokenJWT } = useAuth()
-    const { register, reset, handleSubmit, formState: { errors } } = useForm<AuthForm>({
+    const navigate = useNavigate();
+    const toast = useToast();
+    const { userAuth, setTokenJWT } = useAuth();
+    const {
+        register,
+        reset,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<AuthForm>({
         defaultValues: {
             email: '',
-            password: ''
-        }
-    })
+            password: '',
+        },
+    });
 
     const { mutate, isPending } = useMutation({
         mutationFn: login,
@@ -27,38 +32,46 @@ export default function LoginViews() {
             toast.error(error.message);
         },
         onSuccess: (data) => {
-            if (data) localStorage.setItem('token', data)
-            setTokenJWT(localStorage.getItem('token'))
-            reset()
-            toast.success('Sesión iniciada correctamente')
-            navigate('/ventas')
-        }
-    })
+            if (data) localStorage.setItem('token', data);
+
+            setTokenJWT(localStorage.getItem('token'));
+
+            if (
+                userAuth.roles.name !== Role.admin &&
+                userAuth.roles.name !== Role.employee
+            ) {
+                toast.error(
+                    `El rol ${userAuth.roles.name} no es válido en el sistema. Debe ser uno de los siguientes: ${Role.admin} o ${Role.employee}`,
+                );
+                return;
+            }
+
+            reset();
+            toast.success('Sesión iniciada correctamente');
+            navigate('/ventas');
+        },
+    });
 
     const handleData = (formData: AuthForm) => {
-        mutate(formData)
-    }
+        mutate(formData);
+    };
 
     return (
         <div className="bg-white rounded-md py-10 px-5">
-            <h2 className="text-2xl text-center pb-4 text-cyan-700">Iniciar sesión</h2>
+            <h2 className="text-2xl text-center pb-4 text-cyan-700">
+                Iniciar sesión
+            </h2>
 
             <form
                 onSubmit={handleSubmit(handleData)}
-                className='text-gray-700 flex flex-col justify-around gap-5 '
+                className="text-gray-700 flex flex-col justify-around gap-5 "
                 noValidate
             >
-                <InputEmail
-                    register={register}
-                    errors={errors}
-                />
-                <InputPassword
-                    register={register}
-                    errors={errors}
-                />
+                <InputEmail register={register} errors={errors} />
+                <InputPassword register={register} errors={errors} />
                 <input
                     type="submit"
-                    className='btn-primary'
+                    className="btn-primary"
                     value={'Iniciar sesión'}
                 />
             </form>
@@ -67,13 +80,13 @@ export default function LoginViews() {
             <div className="flex flex-col md:flex-row justify-around gap-4 pt-5">
                 <Link
                     to={'/registrar'}
-                    className='text-sm bg-cyan-8 text-gray-500 hover:text-gray-700 text-center'
+                    className="text-sm bg-cyan-8 text-gray-500 hover:text-gray-700 text-center"
                 >
                     ¿No tienes una cuenta? Registrarse
                 </Link>
                 <Link
                     to={'/olvide-password'}
-                    className='text-sm text-gray-500 hover:text-gray-700 text-center'
+                    className="text-sm text-gray-500 hover:text-gray-700 text-center"
                 >
                     ¿Olvidaste tu contraseña? Restablecer
                 </Link>
