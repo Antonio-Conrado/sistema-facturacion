b9c271bd67846efb3cf239a804b21da048a135aa
Antonio-Conrado <jconradolopez19@gmail.com>
2025-06-30 15:38:15 -0600

    Implemented purchase registration module

--- src/views/app/purchases/RegisterPurchaseView.tsx
+++ src/views/app/purchases/RegisterPurchaseView.tsx
@@ -4,8 +4,22 @@ import { getProductsApi } from '@/api/product/product';
 import Spinner from '@/components/Utils/Spinner';
 import FormPurchases from '../../../components/purchase/registerPurchases/FormPurchases';
 import { getSuppliersAPI } from '@/api/catalogs/supplier';
+import { useAppStore } from '@/store/useAppStore';
+import { ModalKeyList } from '@/types/zustandTypes';
+import { Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { Close } from '@mui/icons-material';
+import InputFileUpload from '@/components/Utils/InputFileUpload';
+import { uploadPurchaseInvoiceAPI } from '@/api/purchase/purchase';
 
 export default function RegisterPurchaseView() {
+    const finalizedPurchaseId = useAppStore(
+        (store) => store.finalizedPurchaseId,
+    );
+
+    const isActiveModal = useAppStore((store) => store.isActiveModal);
+    const activeModalKey = useAppStore((store) => store.activeModalKey);
+    const addedFromModal = useAppStore((store) => store.addedFromModal);
+
     const suppliers = useQuery({
         queryKey: ['suppliers'],
         queryFn: getSuppliersAPI,
@@ -38,6 +52,35 @@ export default function RegisterPurchaseView() {
                         products={products.data}
                     />
                 </div>
+
+                {isActiveModal && activeModalKey === 'purchaseInvoice' && (
+                    <Dialog open={isActiveModal} fullWidth maxWidth="sm">
+                        <div className="relative flex items-center justify-center w-full border-b-2 pb-2">
+                            <DialogTitle className="text-2xl text-center w-4/5">
+                                ¿Desea subir una foto o documento de la factura?
+                            </DialogTitle>
+                            <Close
+                                className="absolute right-4 top-2 text-slate-700 border-orange-800 cursor-pointer"
+                                onClick={() =>
+                                    addedFromModal(
+                                        false,
+                                        ModalKeyList.PurchaseInvoice,
+                                    )
+                                }
+                            />
+                        </div>
+                        <DialogContent className="flex justify-around my-5">
+                            <div className="flex justify-center w-72 mx-auto">
+                                <InputFileUpload
+                                    text="Subir factura"
+                                    infoCache="purchase"
+                                    mutationFn={uploadPurchaseInvoiceAPI}
+                                    id={finalizedPurchaseId}
+                                />
+                            </div>
+                        </DialogContent>
+                    </Dialog>
+                )}
             </>
         );
 }
