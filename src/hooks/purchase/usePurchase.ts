b654f8c95ecaa404782dc52acbcba1f46fe3aad3
Antonio-Conrado <jconradolopez19@gmail.com>
2025-06-30 15:34:53 -0600

    reset form and store finalized purchase ID for invoice upload on success

--- src/hooks/purchase/usePurchase.ts
+++ src/hooks/purchase/usePurchase.ts
@@ -3,19 +3,27 @@ import { useAppStore } from '@/store/useAppStore';
 import { SearchFilterValues } from '@/types/index';
 import { useForm } from 'react-hook-form';
 import useAuth from '../useAuth';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createPurchaseAPI } from '@/api/purchase/purchase';
 import useToast from '../useNotifications';
 import { initialPurchase } from '@/data/index';
 
 export default function usePurchase() {
+    const addedFromModal = useAppStore((store) => store.addedFromModal);
     const [filters, setFilters] = useState<Partial<SearchFilterValues>>({});
 
     const { userAuth } = useAuth();
     const toast = useToast();
     const purchase = useAppStore((store) => store.purchase);
     const addPurchase = useAppStore((store) => store.addPurchase);
+    const resetPurchase = useAppStore((store) => store.resetPurchase);
+    const saveFinalizedPurchaseId = useAppStore(
+        (store) => store.saveFinalizedPurchaseId,
+    );
 
+    const queryClient = useQueryClient();
+
+    // react-hook-form
     const formMethods = useForm({
         defaultValues: {
             ...initialPurchase,
@@ -25,13 +33,26 @@ export default function usePurchase() {
     });
     const { setValue, watch } = formMethods;
 
+    //fetch
     const { mutate } = useMutation({
         mutationFn: createPurchaseAPI,
         onError: (error) => {
             toast.error(error.message);
         },
         onSuccess: (data) => {
-            if (data) toast.success(data);
+            if (data) toast.success(data.msg);
+            // reset data
+            resetPurchase(); //reset store
+            formMethods.reset({
+                ...initialPurchase,
+                usersId: userAuth.id,
+            });
+            setFilters({ suppliersId: 0, ivaId: 0 });
+
+            //upload invoice
+            saveFinalizedPurchaseId(data.purchaseId); //save product ID to upload invoice reference document or photo
+            addedFromModal(true, 'purchaseInvoice');
+            queryClient.invalidateQueries({ queryKey: ['products'] });
         },
     });
 
@@ -57,8 +78,6 @@ export default function usePurchase() {
 
     const handleData = () => {
         const data = { ...purchase, date: new Date(purchase.date) };
-        formMethods.reset();
-        addPurchase(initialPurchase);
         mutate(data);
     };
 
