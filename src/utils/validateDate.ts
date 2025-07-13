4464b2ff6d1bebe653f4d43c0a7dc59cdb2b997f
Antonio-Conrado <jconradolopez19@gmail.com>
2025-07-13 16:32:28 -0600

    Modified getLocalDateString to return yyyy-mm-dd format

--- src/utils/validateDate.ts
+++ src/utils/validateDate.ts
@@ -11,7 +11,8 @@ export default function validateDate(value: string) {
 
 export function getLocalDateString() {
     const now = new Date();
-    const offset = now.getTimezoneOffset();
-    now.setMinutes(now.getMinutes() - offset);
-    return now.toISOString().split('T')[0];
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 }
